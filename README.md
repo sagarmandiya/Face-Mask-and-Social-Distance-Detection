7d685a0cc151e2128cd717138600db9c511f47a3
sagar-mandiya <sagar.mandiya15@gmail.com>
2021-08-30 11:59:24 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Face-Mask-and-Social-Distance-Detection
+### In this project, we bulid a Deep Learning Model based on MobileNetV2 and YOLOv3 to detect live Face Masks and Social Distancing Respectively. 
+
+<br><br>
+
+## Setting up the environment and installing the dependencies
+* ***Open the Terminal**
+* ***Change directory to where you want to downloaded this code***
+* ***Run `git clone https://github.com/sagarmandiya/Face-Mask-and-Social-Distance-Detection.git`**
+* **Creating a new conda environment after installing anaconda**  `  conda create -n ObjectDetection ` ***to create a virtual environment named ObjectDetection.***
+* **Activate the ObjectDetection environment by**   `  conda activate ObjectDetection  python=>3.7` 
+* **Run**   `  pip install -r requirements.txt  ` 
+***to install the python dependencies related to this project like TensorFlow, opencv, numpy, scipy etc.***
+
+<br><br>
+
+## [Optional] Training and Saving the MobileNetV2 model for Face Mask Detecion for Future Inference
+* **This Step can be skipped, as I have already provided my trained model as mask_detector.model which can be directly used for inference purpose**
+* **To Train and Save the model, simply run `python train_mask_detector.py` .** 
+* **Running the above line in terminal will Train the model on the Dataset already provided in the Dataset directory. The dataset can be changed as required just keep the labels same as the directory name in the dataset.**
+
+<br><br>
+
+## To run the inference on local machine:
+* ***Open the terminal**
+* **To Run the detection on a locally stored video named pedestrians.mp4 run** `python live_inference.py --input pedestrians.mp4 --output output.avi --display 1`
+* **Whereas to run the detection live using the Webcam simply run** ` python live_inference.py `
+
+#### **After you run the above line of command, a window will pop up depending upon your choice of live or locally stored video, and after execution of the file an `output.avi` file will be made in your directory if you chose the locally stored video option, otherwise webcam will open for live inference.**
+<br><br>
+
+## Inference on GPU:
+**The same above code can be run with the GPU, but will require but will require OpenCV dnn module to be built on GPU, which can be done as instructed** [here](https://learnopencv.com/opencv-dnn-with-gpu-support/)**. Additionally the code can also be run on google colab to use the GPU for faster training of the MobileNetV2 model for Face Mask Detection.**
+<br><br>
+
+## Contact Me:
+* **Website: [Sagar Mandiya](https://www.sagarmandiya.me)**
+* **Blog: [Sagar Mandiya Blog](https://www.sagarmandiya.me/blog)**
+* **LinkedIn: [Sagar M](https://www.linkedin.com/in/sagar-m-647a2b183)**
+* **Instagram: [Sagar Mandiya](https://www.instagram.com/sagar_mandiya/)**
---
+++ live_inference.py
@@ -0,0 +1,259 @@
+# import the necessary packages for mask-detection
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+
+# import the necessary packages for social-distance-detection
+from modules import social_distancing_config as config
+from modules.detection import detect_people
+from scipy.spatial import distance as dist
+import argparse
+
+import numpy as np
+import imutils
+import time
+import cv2
+import os
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
+	# grab the dimensions of the frame and then construct a blob
+	# from it
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
+		(104.0, 177.0, 123.0))
+
+	# pass the blob through the network and obtain the face detections
+	faceNet.setInput(blob)
+	detections = faceNet.forward()
+	print(detections.shape)
+
+	# initialize our list of faces, their corresponding locations,
+	# and the list of predictions from our face mask network
+	faces = []
+	locs = []
+	preds = []
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with
+		# the detection
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the confidence is
+		# greater than the minimum confidence
+		if confidence > 0.5:
+			# compute the (x, y)-coordinates of the bounding box for
+			# the object
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# ensure the bounding boxes fall within the dimensions of
+			# the frame
+			(startX, startY) = (max(0, startX), max(0, startY))
+			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+			# extract the face ROI, convert it from BGR to RGB channel
+			# ordering, resize it to 224x224, and preprocess it
+			face = frame[startY:endY, startX:endX]
+			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+			face = cv2.resize(face, (224, 224))
+			face = img_to_array(face)
+			face = preprocess_input(face)
+
+			# add the face and bounding boxes to their respective
+			# lists
+			faces.append(face)
+			locs.append((startX, startY, endX, endY))
+
+	# only make a predictions if at least one face was detected
+	if len(faces) > 0:
+		# for faster inference we'll make batch predictions on *all*
+		# faces at the same time rather than one-by-one predictions
+		# in the above `for` loop
+		faces = np.array(faces, dtype="float32")
+		preds = maskNet.predict(faces, batch_size=32)
+
+	# return a 2-tuple of the face locations and their corresponding
+	# locations
+	return (locs, preds)
+
+# load our serialized face detector model from disk
+prototxtPath = r"./face_detector/deploy.prototxt"
+weightsPath = r"./face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# load the face mask detector model from disk
+maskNet = load_model("mask_detector.model")
+
+# initialize the video stream
+print("[INFO] starting video stream...")
+# vs = VideoStream(src=0).start()
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", type=str, default="",
+	help="path to (optional) input video file")
+ap.add_argument("-o", "--output", type=str, default="",
+	help="path to (optional) output video file")
+ap.add_argument("-d", "--display", type=int, default=1,
+	help="whether or not output frame should be displayed")
+args = vars(ap.parse_args())
+# args = vars(ap.parse_args(["--input", "pedestrians.mp4", "--output", "my_output.avi", "--display", "1"]))
+
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.sep.join([config.MODEL_PATH, "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([config.MODEL_PATH, "yolov3.weights"])
+configPath = os.path.sep.join([config.MODEL_PATH, "yolov3.cfg"])
+
+# load our YOLO object detector trained on COCO dataset (80 classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+# check if we are going to use GPU
+if config.USE_GPU:
+	# set CUDA as the preferable backend and target
+	print("[INFO] setting preferable backend and target to CUDA...")
+	net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+	net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# initialize the video stream and pointer to output video file
+print("[INFO] accessing video stream...")
+vs = cv2.VideoCapture(args["input"] if args["input"] else 0)
+writer = None
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	(grabbed, frame) = vs.read()
+	frame = imutils.resize(frame, width=700)
+
+	# if the frame was not grabbed, then we have reached the end
+	# of the stream
+	if not grabbed:
+		break
+
+	# detect faces in the frame and determine if they are wearing a
+	# face mask or not
+	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+
+	# loop over the detected face locations and their corresponding
+	# locations
+	for (box, pred) in zip(locs, preds):
+		# unpack the bounding box and predictions
+		(startX, startY, endX, endY) = box
+		(mask, withoutMask) = pred
+
+		# determine the class label and color we'll use to draw
+		# the bounding box and text
+		label = "Mask" if mask > withoutMask else "No Mask"
+		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+		# include the probability in the label
+		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+		# display the label and bounding box rectangle on the output
+		# frame
+		cv2.putText(frame, label, (startX, startY - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+	# # show the output frame
+	# cv2.imshow("Frame", frame)
+	# key = cv2.waitKey(1) & 0xFF
+
+	# # if the `q` key was pressed, break from the loop
+	# if key == ord("q"):
+	# 	break
+	
+	# read the next frame from the file
+	# (grabbed, frame) = vs.read()
+
+	# resize the frame and then detect people (and only people) in it
+	frame = imutils.resize(frame, width=750)
+	results = detect_people(frame, net, ln,
+		personIdx=LABELS.index("person"))
+
+	# initialize the set of indexes that violate the minimum social
+	# distance
+	violate = set()
+
+	# ensure there are *at least* two people detections (required in
+	# order to compute our pairwise distance maps)
+	if len(results) >= 2:
+		# extract all centroids from the results and compute the
+		# Euclidean distances between all pairs of the centroids
+		centroids = np.array([r[2] for r in results])
+		D = dist.cdist(centroids, centroids, metric="euclidean")
+
+		# loop over the upper triangular of the distance matrix
+		for i in range(0, D.shape[0]):
+			for j in range(i + 1, D.shape[1]):
+				# check to see if the distance between any two
+				# centroid pairs is less than the configured number
+				# of pixels
+				if D[i, j] < config.MIN_DISTANCE:
+					# update our violation set with the indexes of
+					# the centroid pairs
+					violate.add(i)
+					violate.add(j)
+
+	# loop over the results
+	for (i, (prob, bbox, centroid)) in enumerate(results):
+		# extract the bounding box and centroid coordinates, then
+		# initialize the color of the annotation
+		(startX, startY, endX, endY) = bbox
+		(cX, cY) = centroid
+		color = (0, 255, 0)
+
+		# if the index pair exists within the violation set, then
+		# update the color
+		if i in violate:
+			color = (0, 0, 255)
+
+		# draw (1) a bounding box around the person and (2) the
+		# centroid coordinates of the person,
+		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+		cv2.circle(frame, (cX, cY), 5, color, 1)
+
+	# draw the total number of social distancing violations on the
+	# output frame
+	text = "Social Distancing Violations: {}".format(len(violate))
+	cv2.putText(frame, text, (10, frame.shape[0] - 25),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 3)
+
+	# check to see if the output frame should be displayed to our
+	# screen
+	if args["display"] > 0:
+		# show the output frame
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
+
+	# if an output video file path has been supplied and the video
+	# writer has not been initialized, do so now
+	if args["output"] != "" and writer is None:
+		# initialize our video writer
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(args["output"], fourcc, 25,
+			(frame.shape[1], frame.shape[0]), True)
+
+	# if the video writer is not None, write the frame to the output
+	# video file
+	if writer is not None:
+		writer.write(frame)
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
